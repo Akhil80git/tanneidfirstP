9dc3ee5aba0a7a066e51a198b41c00a368437aca
Akhil80git <akhilgarg8085@gmail.com>
2026-01-11 12:09:29 +0530

    hakd

--- Data.js
+++ Data.js
@@ -1,17 +1,63 @@
 module.exports = [
   {
-    "id": "8dbaa4fb-6f36-48be-ba19-27b25e037475",
-    "name": "nmnjfdd",
+    "domain": "rtsad",
+    "school": "hnbi",
+    "name": "sfsf",
     "email": "zancyverma@gmail.com",
-    "school": "45ty",
     "plan": "intermediate",
-    "createdAt": "2026-01-11T03:19:22.444Z"
+    "createdAt": "2026-01-11T06:19:11.471Z",
+    "subdomains": [
+      {
+        "subdomain": "yuhsd",
+        "type": "teacher",
+        "name": "adaf",
+        "description": "ff",
+        "createdAt": "2026-01-11T06:19:29.063Z",
+        "accessLink": "/rtsad/yuhsd"
+      }
+    ]
   },
   {
-    "domain": "Trpsxmd",
-    "school": "hdfhds",
-    "name": "ddd",
-    "plan": "pro",
-    "createdAt": "2026-01-11T03:39:55.908Z"
+    "domain": "qqqq",
+    "school": "qqq",
+    "name": "qqqq",
+    "email": "qq",
+    "plan": "intermediate",
+    "createdAt": "2026-01-11T06:31:06.676Z",
+    "subdomains": []
+  },
+  {
+    "domain": "bbbb",
+    "school": "bbb",
+    "name": "bb",
+    "email": "bbbb",
+    "plan": "intermediate",
+    "createdAt": "2026-01-11T06:36:13.823Z",
+    "subdomains": [
+      {
+        "subdomain": "aswq",
+        "type": "teacher",
+        "name": "asdw",
+        "description": "sdsd",
+        "createdAt": "2026-01-11T06:36:30.859Z",
+        "accessLink": "/bbbb/aswq"
+      },
+      {
+        "subdomain": "sdwa",
+        "type": "student",
+        "name": "asd",
+        "description": "dsa",
+        "createdAt": "2026-01-11T06:37:17.808Z",
+        "accessLink": "/bbbb/sdwa"
+      },
+      {
+        "subdomain": "sdyshfdh",
+        "type": "public",
+        "name": "dfs",
+        "description": "dfsfd",
+        "createdAt": "2026-01-11T06:37:44.293Z",
+        "accessLink": "/bbbb/sdyshfdh"
+      }
+    ]
   }
 ];
